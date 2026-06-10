6fcad166bbc530dd3134dd41b7e4cb43cd75d135
sherpatch <251583033+sherpatch@users.noreply.github.com>
2026-06-10 19:48:06 +0200

    Add sabru formula

---
+++ Formula/sabru.rb
@@ -0,0 +1,21 @@
+class Sabru < Formula
+  desc "Rust CLI for detecting potential subdomain takeover exposure"
+  homepage "https://github.com/sherpatch/subru"
+  url "https://github.com/sherpatch/subru.git",
+      tag:      "v0.1.0",
+      revision: "5b2e12aa489b7ad2d4a4d22109d2aa0e085edc0d"
+  license :cannot_represent
+  head "https://github.com/sherpatch/subru.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bin.install_symlink "subru" => "sabru"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/subru version")
+    assert_match version.to_s, shell_output("#{bin}/sabru version")
+  end
+end
