a56d7eb41a3ab223ee997398e49983b09779b30f
sherpatch <251583033+sherpatch@users.noreply.github.com>
2026-06-10 20:08:04 +0200

    Update subru to 0.1.1

--- Formula/subru.rb
+++ Formula/subru.rb
@@ -2,8 +2,8 @@ class Subru < Formula
   desc "Rust CLI for detecting potential subdomain takeover exposure"
   homepage "https://github.com/sherpatch/subru"
   url "https://github.com/sherpatch/subru.git",
-      tag:      "v0.1.0",
-      revision: "5b2e12aa489b7ad2d4a4d22109d2aa0e085edc0d"
+      tag:      "v0.1.1",
+      revision: "0f133a29d478d9a6ee0d0ceb19bfc5f27694d4fb"
   license :cannot_represent
   head "https://github.com/sherpatch/subru.git", branch: "main"
 
